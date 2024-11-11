5a7da4cf67315016f77b719af2734a92479202e1
Payrouse <leonardobone99@gmail.com>
2024-11-11 10:09:37 -0500

    relaciones personalizadas

---
+++ src/users/services/order-item.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { CreateOrderItemDto } from '../dtos/order-item.dto';
+import { Order } from '../entities/order.entity';
+import { OrderItem } from '../entities/order-item.entity';
+import { Product } from '../../products/entities/product.entity';
+@Injectable()
+export class OrderItemService {
+  constructor(
+    @InjectRepository(Order) private orderRepo: Repository<Order>,
+    @InjectRepository(OrderItem) private itemsRepo: Repository<OrderItem>,
+    @InjectRepository(Product) private productRepo: Repository<Product>,
+  ) {}
+  async create(data: CreateOrderItemDto) {
+    const order = await this.orderRepo.findOne({ where: { id: data.orderId } });
+    const product = await this.productRepo.findOne({
+      where: { id: data.productId },
+    });
+    const item = new OrderItem();
+    item.order = order;
+    item.product = product;
+    item.quantity = data.quantity;
+    return this.itemsRepo.save(item);
+  }
+}
--- src/users/services/orders.service.ts
+++ src/users/services/orders.service.ts
@@ -2,9 +2,9 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
-import { Order } from '../entities/order.entity';
-import { Customer } from '../entities/customer.entity';
-import { CreateOrderDto, UpdateOrderDto } from '../dtos/order.dto';
+import { Order } from './../entities/order.entity';
+import { Customer } from './../entities/customer.entity';
+import { CreateOrderDto, UpdateOrderDto } from './../dtos/order.dto';
 
 @Injectable()
 export class OrdersService {
@@ -18,9 +18,12 @@ export class OrdersService {
   }
 
   async findOne(id: number) {
-    const order = await this.orderRepo.findOne({ where: { id: id } });
-    if (order!) {
-      throw new NotFoundException('Not Found');
+    const order = await this.orderRepo.findOne({
+      where: { id },
+      relations: ['items', 'items.product'],
+    });
+    if (!order) {
+      throw new NotFoundException('not found');
     }
     return order;
   }
@@ -33,11 +36,11 @@ export class OrdersService {
       });
       order.customer = customer;
     }
-    return this.customerRepo.save(order);
+    return this.orderRepo.save(order);
   }
 
   async update(id: number, changes: UpdateOrderDto) {
-    const order = await this.orderRepo.findOne({ where: { id: id } });
+    const order = await this.orderRepo.findOne({ where: { id } });
     if (changes.customerId) {
       const customer = await this.customerRepo.findOne({
         where: { id: changes.customerId },
